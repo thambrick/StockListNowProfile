619545fa7778b71a3a939ae5f2935a1d0ca7869e
treyhambrick <trey.hambrick@cox.net>
2021-03-09 10:08:02 -0500

    add auth

--- src/index.js
+++ src/index.js
@@ -3,16 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Amplify from 'aws-amplify';
-import config from './aws-exports';
-Amplify.configure(config);
+//import Amplify from 'aws-amplify';
+//import config from './aws-exports';
+//Amplify.configure(config);
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+//ReactDOM.render(
+//  <React.StrictMode>
+//    <App />
+//  </React.StrictMode>,
+//  document.getElementById('root')
+//);
+
+const myelement = (
+                <center><h2><label id='profileTitle'>Complete your Profile</label></h2>
+				<form  id='profileForm' method='post'>					
+				<table>
+				<tr><td>DisplayName For SLN..... </td><td><input type='text' value='' id='displayName'/></td></tr>
+				<tr><td>City</td><td><input type='text' value='' id='city'/></td></tr>
+				<tr><td>Email</td><td><input type='text' value='' id='emailLabel'/></td></tr>
+			    <tr><td colspan='2' align='center'>
+				<input type='button' value='Submit' id='updateProfile'  onClick="alert('trey')"/> </td></tr>
+			    </table>
+                </form> </center>
 );
+
+ReactDOM.render(myelement, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
