eae49433ede53c5fd33a62affc76455f9150ca2e
treyhambrick <trey.hambrick@cox.net>
2021-03-11 12:48:15 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -14,8 +14,35 @@ class Container extends React.Component {
     //this.state = {show: true};
   //}
   updateProfile = () => {
-	alert("updated Profile.");  
+	alert("updated Profile."); 
+    alert(document.getElementById("displayName").value);	
     //this.setState({show: false});
+	/*
+	let userId = "testuserId";
+	let emailLabel = document.getElementById("emailLabel").value;
+	let displayName = document.getElementById("displayName").value;
+	let city = document.getElementById("city").value;
+	
+	    //let test = callUpdateUserAPI = ("testuserId", document.getElementById("emailLabel").value, document.getElementById("displayName").value, document.getElementById("city").value)=>{
+        let test = callUpdateUserAPI = (userId, email, displayName, city)=>{
+          
+   		    var myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
+            var raw = JSON.stringify({"userId":userId,"email":email,"displayName":displayName,"city":city});
+            var requestOptions = {
+                method: 'POST',
+                headers: myHeaders,
+                body: raw,
+                redirect: 'follow'
+            };
+            fetch("https://u3yyrwv2r5.execute-api.us-west-2.amazonaws.com/dev", requestOptions)
+             .then(response => response.text())
+             //.then(result => document.getElementById('getDataInputLabel').innerHTML=JSON.parse(result).body)
+             //.then(result => setUpdateUserData(JSON.parse(result).body))
+			 .then(result => alert(JSON.parse(result).body))
+            .catch(error => alert(JSON.parse(error).body));
+        }
+		*/
   }
   render() {
     //let myheader;
