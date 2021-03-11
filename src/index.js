d6d0963c0f4f53d8117374f0bf8658166ca2d15e
treyhambrick <trey.hambrick@cox.net>
2021-03-11 11:20:48 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -5,46 +5,41 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './main.css';
+
 
 class Container extends React.Component {
-  constructor(props) {
-    super(props);
+  //constructor(props) {
+  //  super(props);
     //this.state = {show: true};
-  }
+  //}
   updateProfile = () => {
 	alert("updated Profile.");  
     //this.setState({show: false});
   }
   render() {
-    let myheader;
+    //let myheader;
     //if (this.state.show) {
     //  myheader = <Child />;
     //};
     return (
-                <center>{myheader}
+                <center>
+			    <div style={{ width: 600, }}>
 				<h2><label id='profileTitle'>Complete your Profile</label></h2>
-				<form  id='profileForm' method='post' action22={this.updateProfile} action="www.stocklistnow.com">					
-				<table>
-				<tr><td>DisplayName </td><td><input type='text' value='' id='displayName'/></td></tr>
-				<tr><td>City</td><td><input type='text' value='' id='city'/></td></tr>
-				<tr><td>Email</td><td><input type='text' value='' id='emailLabel'/></td></tr>
+				<form  id='profileForm' method='post' >					
+				<table > 
+				<tr><td>DisplayName </td><td><input type='text'  id='displayName' onchange={this.handleChange}/></td></tr>
+				<tr><td>City</td><td><input type='text'  id='city' onchange={this.handleChange}/></td></tr>
+				<tr><td>Email</td><td><input type='text'  id='emailLabel' onchange={this.handleChange}/></td></tr>
 			    <tr><td colspan='2' align='center'>
-				<input type='button' value='Submit' id='updateProfile'  onClick={this.updateProfile}/> </td></tr>
+				<input type='button' value="Submit" id='updateProfile'  onClick={this.updateProfile}/> </td></tr>
 			    </table>
-                </form> </center>
+                </form>
+              	</div>		
+				</center> 
         );
   }
 }
 
-class Child extends React.Component {
-  componentWillUnmount() {
-    alert("The component named Header is about to be unmounted.");
-  }
-  render() {
-    return (
-      <h1>Header!</h1>
-    );
-  }
-}
 
 ReactDOM.render(<Container />, document.getElementById('root'));
--- src/old/App.js
+++ src/old/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import 'main.css';
+		
+//import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
