014918f7e9067cfa00fefd8b45ebe76352c04375
treyhambrick <trey.hambrick@cox.net>
2021-03-11 17:48:24 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -53,7 +53,7 @@ class Container extends React.Component {
 	//</div>
     return (    
                 <div class="inner">
-				<h2><label id='profileTitle'>Complete your Profile...</label></h2>
+				<h2><label id='profileTitle'>Complete your Profile</label></h2>
 				<form id='profileForm' method='post' >					
 				<table > 
 				<tr><td>DisplayName </td><td><input type='text' id='displayName' onchange={this.handleChange}/></td></tr>
