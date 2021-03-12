2f0b7372ea1d0eeb77eba8d62538fddcbfe4d8e0
treyhambrick <trey.hambrick@cox.net>
2021-03-12 14:50:10 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -7,7 +7,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './main.css';
+import queryString from 'query-string';
 //import pic01 from './images/pic01.jpg'; // with import
+
+
 
 function setData(data){
    alert(data);
@@ -17,10 +20,10 @@ function setData(data){
    //alert(document.getElementById("email").value);
 	
 	//window.close();
-    createCookieInHour("displayName",document.getElementById("displayName").value,9000);	
-	createCookieInHour("city",document.getElementById("city").value,9000);
-	createCookieInHour("email",document.getElementById("email").value,9000);
-	window.parent.getElementById('centerDiv').style.display = "none";	
+    //createCookieInHour("displayName",document.getElementById("displayName").value,9000);	
+	//createCookieInHour("city",document.getElementById("city").value,9000);
+	//createCookieInHour("email",document.getElementById("email").value,9000);
+	//window.parent.getElementById('centerDiv').style.display = "none";	
 	//let email = document.getElementById("email").value;
 	//let displayName = document.getElementById("displayName").value;
 	//let city = document.getElementById("city").value;
@@ -36,15 +39,20 @@ function createCookieInHour (cookieName, cookieValue, hourToExpire) {
     document.cookie = cookieName + " = " + cookieValue + "; expires = " +date.toGMTString();
 }
 
+var userId;
 
 class Container extends React.Component {
   constructor(props) {
     super(props);
 	//let userId2 = document.cookie.replace(/(?:(?:^|.*;\s*)userId\s*\=\s*([^;]*).*$)|^.*$/, "$1"); 
 	//parent.iFrameWin = window;
-	alert("in react, this.props.location.search="); 
-	alert(this.props.location.search);
-	alert(location.search);
+	alert("in react, userId="); 
+	
+	//alert(this.props.location.search);
+	let params = window.location.search;
+	userId = params.replace("?userId=", "");
+	alert(userId);
+	//alert(location.search);
 	//let email2 = document.cookie.replace(/(?:(?:^|.*;\s*)email\s*\=\s*([^;]*).*$)|^.*$/, "$1");
 	//alert(email2);
     //this.state = {show: true};
@@ -53,7 +61,7 @@ class Container extends React.Component {
 	//alert("updated Profile."); 
     //alert(document.getElementById("displayName").value);	
     //this.setState({show: false});
-		let userId = document.cookie.replace(/(?:(?:^|.*;\s*)userId\s*\=\s*([^;]*).*$)|^.*$/, "$1"); 
+		//let userId = document.cookie.replace(/(?:(?:^|.*;\s*)userId\s*\=\s*([^;]*).*$)|^.*$/, "$1"); 
 		if (!userId){ 
 		   alert("you must login before completing your profile"); 
 		   return false;
@@ -61,7 +69,8 @@ class Container extends React.Component {
 		
 		try {
 			//let userId = document.cookie.replace(/(?:(?:^|.*;\s*)userId\s*\=\s*([^;]*).*$)|^.*$/, "$1"); 
-			userId = "ttteesssttt";
+			alert("userId===");
+			alert(userId);
 			let email = document.getElementById("email").value;
 			let displayName = document.getElementById("displayName").value;
 			let city = document.getElementById("city").value;
@@ -69,6 +78,7 @@ class Container extends React.Component {
             var myHeaders = new Headers();
             myHeaders.append("Content-Type", "application/json");
             var raw = JSON.stringify({"userId":userId,"email":email,"displayName":displayName,"city":city});
+			alert(raw);
             var requestOptions = {
                 method: 'POST',
                 headers: myHeaders,
@@ -77,8 +87,8 @@ class Container extends React.Component {
             };
 			fetch("https://u3yyrwv2r5.execute-api.us-west-2.amazonaws.com/dev", requestOptions)
              .then(response => response.text())
-			 //.then(result => alert(JSON.parse(result).body))
-			 .then(result => setData(JSON.parse(result).body))
+			 .then(result => alert(JSON.parse(result).body))
+			 //.then(result => setData(JSON.parse(result).body))
             .catch(error => alert(JSON.parse(error).body));
 		} catch (error) {
 			//console.error(error);
@@ -95,7 +105,8 @@ class Container extends React.Component {
 	//</div>
 	//<img src={pic01} alt="" data-position="top center" />
 	//<a href="#yourProfile" class="image"><img src={pic01} alt="" data-position="top center" /></a>
-	let email = document.cookie.replace(/(?:(?:^|.*;\s*)email\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+	///let email = document.cookie.replace(/(?:(?:^|.*;\s*)email\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+	//<tr><td>Current Email</td><td>{email}</td></tr>
     return (    
 	            <section id="yourProfile" class="wrapper style2 spotlights">
 	            <section>
@@ -108,7 +119,6 @@ class Container extends React.Component {
 				<tr><td>DisplayName </td><td><input type='text' id='displayName' onchange={this.handleChange}/></td></tr>
 				<tr><td>City</td><td><input type='text' id='city' onchange={this.handleChange}/></td></tr>
 				<tr><td>Email</td><td><input type='text' id='email' onchange={this.handleChange}/></td></tr>
-				<tr><td>Current Email</td><td>{email}</td></tr>
 			    <tr><td colspan='2' align='center'>
 				<input type='button' value="Submit" id='updateProfile' onClick={this.updateProfile}/> </td></tr>
 			    </table>
